62d2f47daff8e828719d6e001d4e64ea3b6e273e
Ian Muchina <muchinaofficial@gmail.com>
2020-05-01 18:02:55 +0300

    Update all.sh

--- src/all.sh
+++ src/all.sh
@@ -5,8 +5,6 @@ alias a2path='echo "export PATH=$PWD:\$PATH" >> ~/.bashrc'
 alias gh='history|grep'
 alias rebash='source ~/.bashrc'
 # Prompts
-
-# Modern Prompt 
 if [ $(id -u) -eq 0 ];
 then # I am root, make the prompt different
     PS1="\[\033[38;5;7m\][\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;13m\]\@\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;7m\]]\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;9m\]\u@\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\w\[$(tput sgr0)\] \\$\n\[$(tput sgr0)\]\[\033[38;5;13m\]❯\[$(tput sgr0)\]\[\033[38;5;9m\]❯\[$(tput sgr0)\]\[\033[38;5;14m\]❯\[$(tput sgr0)\] \[$(tput sgr0)\]"
